117e6d590d7bded1405f5c9454b2bd6881e0ed75
Punarv Pawade <punarvpawade17@gmail.com>
2024-01-19 03:23:54 +0530

    username integration

--- contracts/Split.sol
+++ contracts/Split.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.9;
 
 contract MultiWallet {
     address public owner;
-    mapping(address => uint256) public balances;
+    address public temporaryAddress; // Temporary address to store funds if wallet is not assigned
+    mapping(string => address) public usernameToAddress;
+    mapping(address => mapping(string => uint256)) public balances;
 
     constructor() {
         owner = msg.sender;
@@ -17,33 +19,61 @@ contract MultiWallet {
         _;
     }
 
+    function assignAddressToUsername(
+        string memory username,
+        address walletAddress
+    ) public onlyOwner {
+        usernameToAddress[username] = walletAddress;
+    }
+
     function deposit(
-        address[] memory receivers,
+        string[] memory usernames,
         uint256[] memory amounts
     ) public payable onlyOwner {
         require(
-            receivers.length == amounts.length,
-            "Receiver and amount arrays must have the same length"
+            usernames.length == amounts.length,
+            "Arrays must have the same length"
         );
-        uint256 amt = 0;
-        for (uint256 index = 0; index < amounts.length; index++) {
-            amt += amounts[index];
-        }
-        require(amt == msg.value, "insufficient value");
-        for (uint256 i = 0; i < receivers.length; i++) {
-            address receiver = receivers[i];
+
+        for (uint256 i = 0; i < usernames.length; i++) {
+            string memory username = usernames[i];
             uint256 amount = amounts[i];
-            balances[receiver] += amount;
+
+            address receiver = usernameToAddress[username];
+
+            if (receiver == address(0)) {
+                // Wallet address not assigned, store funds to temporary address
+                temporaryAddress = address(this);
+                balances[temporaryAddress][username] += amount;
+            } else {
+                // Wallet address assigned, transfer funds directly
+                require(
+                    amount == msg.value,
+                    "Sent value must match the specified amount"
+                );
+                balances[receiver][username] += amount;
+                payable(receiver).transfer(amount);
+            }
         }
     }
 
-    function withdraw(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
+    function withdraw(string memory username, uint256 amount) public {
+        address receiver = usernameToAddress[username];
+        require(
+            receiver == msg.sender,
+            "You can only withdraw funds for your assigned username"
+        );
+        require(balances[receiver][username] >= amount, "Insufficient balance");
+        balances[receiver][username] -= amount;
         payable(msg.sender).transfer(amount);
     }
 
-    function getBalance(address account) public view returns (uint256) {
-        return balances[account];
+    function getBalance(string memory username) public view returns (uint256) {
+        address receiver = usernameToAddress[username];
+        if (receiver == address(0)) {
+            return balances[temporaryAddress][username];
+        } else {
+            return balances[receiver][username];
+        }
     }
 }
